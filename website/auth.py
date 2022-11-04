ba5ff0450a715e23eb8fc54dd90bc9e657870aa2
zgonx <marcin2000c@gmail.com>
2022-11-04 13:14:33 +0100

    +notes

--- website/auth.py
+++ website/auth.py
@@ -59,7 +59,7 @@ def sign_up():
                             password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
     return render_template("sign_up.html", user=current_user)
